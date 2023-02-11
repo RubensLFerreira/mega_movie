779d27bdf1404b52376a5dbab4f56c680c5c5f0b
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-10 21:26:14 -0300

    pages de resultados

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import tmdb from "../services/tmdb";
+import MovieCard from "../components/MovieCard";
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const key = import.meta.env.VITE_API_KEY;
+
+import "./MoviesGrid.css";
+
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchMovies = async (url) => {
+    const movies = await tmdb.get(url).then((res) => res);
+    setMovies(movies.data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${searchURL}?${key}&query=${query}&language=pt-BR`;
+    getSearchMovies(searchWithQueryURL);
+  }, [query]);
+
   return (
-    <h2>Search</h2>
+    <div className="container">
+      <h2 className="title">
+        Resultados para: <span className="query-text">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   );
 };
 
