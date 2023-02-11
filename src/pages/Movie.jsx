70e2cc4b01da7005d7881364cbb55ae34223e1d4
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-10 21:24:39 -0300

    especificações do filme

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,77 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import tmdb from "../services/tmdb";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const key = import.meta.env.VITE_API_KEY;
+
+import "./Movie.css";
+
 const Movie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (url) => {
+    const movies = await tmdb.get(url).then((res) => res);
+    setMovie(movies.data);
+  };
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  useEffect(() => {
+    const movieURL = `${moviesURL}${id}?${key}&language=pt-BR`;
+    getMovie(movieURL);
+  }, []);
+
   return (
-    <h2>Movie</h2>
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>{formatCurrency(movie.budget)}</p>
+          </div>
+
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+          </div>
+
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime} minutos</p>
+          </div>
+
+          <div className="info description">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
