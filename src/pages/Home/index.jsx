0b9ebdada6e2547406ba24f81ed7446d3f28b2aa
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-11 20:58:22 -0300

    Home screen in progress

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,38 +1,28 @@
-import { useState, useEffect } from "react";
+import "./style.css";
 
-import tmdb from "../../services/tmdb";
-import MovieCard from "../../components/MovieCard";
-
-import './style.css';
-
-const moviesURL = import.meta.env.VITE_API;
-const key = import.meta.env.VITE_API_KEY;
+import wallpaper from "/wallpaper/galaxia.jpg";
 
 const Home = () => {
-  const [topMovies, setTopMovies] = useState([]);
-
-  // pegando lista de filmes
-  const getTopRatedMovies = async (url) => {
-    const movies = await tmdb.get(url).then((res) => res);
-    // const data = await res.json();
-    // console.log(movies.data.results);
-    setTopMovies(movies.data.results);
-  };
-
-  useEffect(() => {
-    // top filmes avaliados
-    const topRatedUrl = `${moviesURL}top_rated?${key}&language=pt-BR`;
-    // console.log(topRatedUrl);
-    getTopRatedMovies(topRatedUrl);
-  }, []);
-
   return (
     <div className="container">
-      <h2 className="title">Top filmes</h2>
-      <div className="movies-container">
-        {/* cache */}
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+      {/* <h2 className="title">Início</h2> */}
+      <div>
+        <img className="wallpaper" src={wallpaper} alt="wallpaper" />
+      </div>
+      <div className="mega-movie">
+        <h1>
+          Bem vindo ao <span>Mega Movie</span>
+        </h1>
+      </div>
+
+      <div className="lancamentos">
+        <div>
+          <h2>Filmes em cartaz</h2>
+        </div>
+
+        <div>
+          <h2>Séries em cartaz</h2>
+        </div>
       </div>
     </div>
   );
