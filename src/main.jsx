dcfa6f89cf7b7e010fa8e529d379c3525a87a150
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-11 21:03:17 -0300

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Home from "./pages/Home";
+
+import TopMovies from "./pages/TopMovies";
 import Movie from "./pages/Movie";
+
+import TopSeries from "./pages/TopSeries";
+import Serie from "./pages/Serie";
+
+import TopPeople from "./pages/TopPeople";
 import Search from "./pages/Search";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,7 +20,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Home />} />
+
+          <Route path="/topmovies" element={<TopMovies />} />
           <Route path="/movie/:id" element={<Movie />} />
+
+          <Route path="/topseries" element={<TopSeries />} />
+          <Route path="/tv/:id" element={<Serie />} />
+
+          <Route path="/toppeople" element={<TopPeople />} />
+
           <Route path="search" element={<Search />} />
         </Route>
       </Routes>
