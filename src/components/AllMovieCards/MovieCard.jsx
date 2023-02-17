0ed0ae577cb3413f50a72a4e7653f1073182de3b
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-17 15:17:46 -0300

    adding all movie cards

---
+++ src/components/AllMovieCards/MovieCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+import { FaStar } from "react-icons/fa";
+
+const imageUrl = import.meta.env.VITE_IMG;
+
+function MovieCard({ movie, showLink = true }) {
+  return (
+    <div className="card text-bg-dark">
+      <img
+        src={imageUrl + movie.poster_path}
+        className="card-img-top"
+        alt={movie.title}
+      />
+      <div className="card-body">
+        <h5 className="card-title">{movie.title}</h5>
+        <p className="card-text">
+          <FaStar /> {movie.vote_average}
+        </p>
+        {showLink && (
+          <Link to={`/movie/${movie.id}`} className="btn btn-primary">
+            Detalhes
+          </Link>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
