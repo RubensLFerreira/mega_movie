d4daaa77471db2ecd4597a49644275fa6a04ab9f
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-17 16:03:12 -0300

    primary app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
