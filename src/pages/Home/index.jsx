8c527d4d205836aa8c8c8695151a37d4669c9154
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-17 16:01:44 -0300

    home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,21 @@
-import NowPlaying from "../NowPlayingMovie";
-import Wallpaper from '../../components/Wallpaper';
+import Wallpaper from "../../components/Wallpaper";
+import MoviesNowPlaying from "../AllMovies/MoviesNowPlaying";
+
+import "./HomeStyle.css";
 
 const Home = () => {
   return (
     <div>
       <Wallpaper />
-      <NowPlaying />
+      <div className="container">
+        <h1 className="title-primary">
+          Bem-vindo ao <span>Mega Movie</span>
+        </h1>
+        <h3 className="title-second">
+          Milhões de filmes, séries para descobrir. Venha explorar!
+        </h3>
+      </div>
+      <MoviesNowPlaying />
     </div>
   );
 };
