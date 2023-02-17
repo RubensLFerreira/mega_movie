5432bbd9b5db7b013e085281a2e986a11d0fcd67
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2023-02-17 16:03:47 -0300

    all routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,31 +5,44 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 
-import TopMovies from "./pages/TopMovies";
-import Movie from "./pages/Movie";
+import Movie from "./pages/AllMovies/MoviesDetails";
+import MoviesTopRated from "./pages/AllMovies/MoviesTopRated";
+import MoviesPopular from "./pages/AllMovies/MoviesPopular";
+import MoviesNowPlaying from "./pages/AllMovies/MoviesNowPlaying";
+import MoviesUpComing from "./pages/AllMovies/MoviesUpComing";
 
-import TopSeries from "./pages/TopSeries";
-import Serie from "./pages/Serie";
+import Serie from './pages/AllSeries/SeriesDetails';
+import SeriesTopRated from "./pages/AllSeries/SeriesTopRated";
+import SeriesPopular from "./pages/AllSeries/SeriesPopular";
+import SeriesAiringToday from "./pages/AllSeries/SeriesAiringToday";
+import SeriesOnTheAir from "./pages/AllSeries/SeriesOnTheAir";
 
-import TopPeople from "./pages/TopPeople";
 import Search from "./pages/Search";
+import People from './pages/People';
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}></Route>
+          <Route path="/movie/:id" element={<Movie />}></Route>
+          <Route path="/movies/top_rated" element={<MoviesTopRated />}></Route>
+          <Route path="/movies/popular" element={<MoviesPopular />}></Route>
+          <Route path="/movies/now_playing" element={<MoviesNowPlaying />}></Route>
+          <Route path="/movies/upcoming" element={<MoviesUpComing />}></Route>
 
-          <Route path="/topmovies" element={<TopMovies />} />
-          <Route path="/movie/:id" element={<Movie />} />
+          <Route path="/tv/:id" element={<Serie />}></Route>
+          <Route path="/tv/top_rated" element={<SeriesTopRated />}></Route>
+          <Route path="/tv/popular" element={<SeriesPopular />}></Route>
+          <Route path="/tv/airing_today" element={<SeriesAiringToday />}></Route>
+          <Route path="/tv/on_the_air" element={<SeriesOnTheAir />}></Route>
+        
+          <Route path="search" element={<Search />}></Route>
 
-          <Route path="/topseries" element={<TopSeries />} />
-          <Route path="/tv/:id" element={<Serie />} />
-
-          <Route path="/toppeople" element={<TopPeople />} />
-
-          <Route path="search" element={<Search />} />
+          <Route path="/people" element={<People />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
